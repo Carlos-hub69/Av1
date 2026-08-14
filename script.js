d43f2716a0f3e0dcdbacbe275248165b87c91b5f
Carlos-hub69 <carlos.neves29@escola.pr.gov.br>
2026-08-14 09:41:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+
+    loginForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        const email =
+            document.getElementById("email").value;
+
+        const senha =
+            document.getElementById("senha").value;
+
+        if (email && senha) {
+
+            localStorage.setItem(
+                "usuario",
+                email
+            );
+
+            window.location.href = "index.html";
+
+        } else {
+
+            alert("Preencha todos os campos.");
+
+        }
+
+    });
+
+}
